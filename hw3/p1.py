fbe221da2686e8a6d6d1d5a7d9ab2454abe2e78f
aaryadevchandra <aaryadevc@gmail.com>
2026-03-14 23:46:13 -0400

    finding avgs

--- hw3/p1.py
+++ hw3/p1.py
@@ -120,3 +120,149 @@ print("avg chain length =", x.average_chain_length())
 print("max chain length =", x.max_chain_length())
 print()
 
+
+table_sizes = [101, 503, 1009]
+load_factors = [0.25, 0.50, 0.75, 1.00, 1.25, 1.50]
+
+all_success = {}
+all_unsuccess = {}
+
+for s in table_sizes:
+    success_times = []
+    unsuccess_times = []
+
+    for lf in load_factors:
+        num_items = int(s * lf)
+
+        keys = []
+        for i in range(num_items):
+            keys.append(random.randint(0, 1000000))
+
+        h = HashTable(s, "division")
+
+        for i in range(len(keys)):
+            h.insert(keys[i], "val" + str(keys[i]))
+
+        total1 = 0.0
+        for i in range(1000):
+            key = random.choice(keys)
+            start = time.perf_counter()
+            h.search(key)
+            end = time.perf_counter()
+            total1 = total1 + (end - start)
+
+        avg1 = total1 / 1000
+
+        key_set = set(keys)
+        total2 = 0.0
+        count = 0
+        while count < 1000:
+            key = random.randint(1000001, 3000000)
+            if key not in key_set:
+                start = time.perf_counter()
+                h.search(key)
+                end = time.perf_counter()
+                total2 = total2 + (end - start)
+                count = count + 1
+
+        avg2 = total2 / 1000
+
+        success_times.append(avg1)
+        unsuccess_times.append(avg2)
+
+        print("size", s, "load factor", lf)
+        print("successful", avg1)
+        print("unsuccessful", avg2)
+        print("avg chain", h.average_chain_length())
+        print("max chain", h.max_chain_length())
+        print()
+
+    all_success[s] = success_times
+    all_unsuccess[s] = unsuccess_times
+
+plt.figure(figsize=(8, 5))
+for s in table_sizes:
+    plt.plot(load_factors, all_success[s], marker="o", label="size=" + str(s))
+plt.xlabel("Load Factor")
+plt.ylabel("Average Successful Search Time")
+plt.title("Load Factor vs Successful Search Time")
+plt.legend()
+plt.grid(True)
+plt.show()
+
+plt.figure(figsize=(8, 5))
+for s in table_sizes:
+    plt.plot(load_factors, all_unsuccess[s], marker="o", label="size=" + str(s))
+plt.xlabel("Load Factor")
+plt.ylabel("Average Unsuccessful Search Time")
+plt.title("Load Factor vs Unsuccessful Search Time")
+plt.legend()
+plt.grid(True)
+plt.show()
+
+
+names = ["Uniform", "Skewed", "Sequential"]
+success_result = []
+unsuccess_result = []
+avg_chain_result = []
+max_chain_result = []
+
+table_size = 1009
+num_items = table_size
+
+for z in range(3):
+    keys = []
+
+    if z == 0:
+        for i in range(num_items):
+            keys.append(random.randint(0, 1000000))
+
+    elif z == 1:
+        for i in range(num_items):
+            r = random.random()
+            keys.append(int((r ** 3) * 1000000))
+
+    else:
+        for i in range(num_items):
+            keys.append(i)
+
+    h = HashTable(table_size, "multiplication")
+
+    for i in range(len(keys)):
+        h.insert(keys[i], "val" + str(keys[i]))
+
+    total1 = 0.0
+    for i in range(1000):
+        key = random.choice(keys)
+        start = time.perf_counter()
+        h.search(key)
+        end = time.perf_counter()
+        total1 = total1 + (end - start)
+
+    avg1 = total1 / 1000
+
+    key_set = set(keys)
+    total2 = 0.0
+    count = 0
+    while count < 1000:
+        key = random.randint(1000001, 3000000)
+        if key not in key_set:
+            start = time.perf_counter()
+            h.search(key)
+            end = time.perf_counter()
+            total2 = total2 + (end - start)
+            count = count + 1
+
+    avg2 = total2 / 1000
+
+    success_result.append(avg1)
+    unsuccess_result.append(avg2)
+    avg_chain_result.append(h.average_chain_length())
+    max_chain_result.append(h.max_chain_length())
+
+    print(names[z])
+    print("successful", avg1)
+    print("unsuccessful", avg2)
+    print("avg chain", h.average_chain_length())
+    print("max chain", h.max_chain_length())
+    print()
